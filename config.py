28c36e55e24af08934ffce22f8960808ccf02220
Vipin Kumar <vipin275869@gmail.com>
2026-03-21 20:15:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-SOURCE = getenv("SOURCE", "")
+SOURCE = getenv("SOURCE", "https://github.com/dilchorofficial/Jack")
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/dilchorofficial/Jack",
